a712dd09171ffc4d4d39b02a1181e98e2601ab2d
Tero Hannula <terohan@gmail.com>
2017-07-28 15:29:15 +0300

    Navigation works finally, gallery added but broken

--- js/modules.js
+++ js/modules.js
@@ -1,43 +1,79 @@
+
+/*-----------------------------------------------------
+
+The AngularJS module for the course assignment
+
+Author: Tero Hannula (terohan@gmail.com)
+
+------------------------------------------------------*/
+
+
 // create the module and name it
 
 var assignment_module = angular.module('app', ['ngRoute']);
 
-assignment_module.config(function($routeProvider) { // configure routes for navigation
+// configure routes for navigation
 
-		$routeProvider.when('/', {
-				templateUrl : 'index.html'
+assignment_module.config(['$routeProvider', function($routeProvider) { 
+
+		$routeProvider.when("/", {
+				templateUrl : "home.html"
 			})
             
-            .when('/projects', {
-				templateUrl : 'projects.html'
+            .when("/projects", {
+				templateUrl : "projects.html"
             })
             
-            .when('/contact', {
-				templateUrl : 'contact.html'
+            .when("/contact", {
+				templateUrl : "contact.html"
 			})
 
-			.when('/links', {
-				templateUrl : 'links.html'
+			.when("/links", {
+				templateUrl : "links.html"
+			})
+
+			.when("/gallery", {
+				templateUrl : "gallery.html"
 			})
             
-			.when('/skills', {
-				templateUrl : 'skills.html'
-			});
-    })
+			.when("/skills", {
+				templateUrl : "skills.html"
+			})
 
-assignment_module.controller("mainCtrl", ['$scope', function($scope) {
-            
-    $scope.a=0;
-    
-}]);
+			.otherwise({
+                    redirectTo: "/"
+            });
+    }]);
 
-assignment_module.controller('TimeCtrl', ['$scope', '$interval', function($scope, $interval) {
-  
-	// run the clock in the footer
+
+
+// The only controller in this app
+
+assignment_module.controller("mainCtrl", ['$scope', '$interval', function($scope, $interval) {
+
+	// run the clock in the footer	
 	var tick = function() {
     	$scope.clock = Date.now();
   	}
   	tick();
-  	$interval(tick, 1000);
+	$interval(tick, 1000); 
+
+	
+	
+	// Nav bar related functions
+
+	$scope.selectedpage='home';
+
+	$scope.setSelected = function(selected) {
+		$scope.selectedpage = selected;
+	}
+
+	$scope.getSelected = function(suggestion) {
+		if(suggestion == $scope.selectedpage) 
+			return true;
+		else 
+			return false;
+		
+	}
 
 }]);
